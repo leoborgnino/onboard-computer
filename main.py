2897867f62323e0a10a4f7b13020118af6e3e1ca
leoborgnino <leo.borgnino@gmail.com>
2015-04-12 14:42:18 -0300

    Agregada clase graphic y clase server.Falta vincular el main con las clases y pensar la transmisión entre micro y pc con los datos del acelerometro.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import Comunicacion
 import time
+import graphic
 
 com = Comunicacion.Comunicacion('/dev/ttyS0',115200)
 com.open()
 com.send("defghijklmn",10)
 time.sleep(5)
 com.close()
+
+grafico = graphic.grafico()
+grafico.run()
+
+while True:
+	pass
