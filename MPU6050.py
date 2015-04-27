ef282686d6b41ccfb095919ac079652566ff64c6
ingenia <ingenia@ingenia>
2015-04-27 17:26:06 -0300

    Funcionando MPU6050.py

--- MPU6050.py
+++ MPU6050.py
@@ -18,32 +18,26 @@ class mpu6050(Obj_Head):
                 self.__arreglo_datos = []
         
         def save(self,datos):
-                self.__x_acel = struct.unpack('<f',struct.pack('4b', *datos[0:4]))[0]
-                self.__y_acel = struct.unpack('<f',struct.pack('4b', *datos[5:9]))[0]
-                self.__z_acel = struct.unpack('<f',struct.pack('4b', *datos[10:14]))[0]
-                self.__x_gyro = struct.unpack('<f',struct.pack('4b', *datos[15:19]))[0]
-                self.__y_gyro = struct.unpack('<f',struct.pack('4b', *datos[20:24]))[0]
-                #self.__z_gyro = struct.unpack('<f',struct.pack('4b', *datos[25:29]))[0]
+                for j in range (6):
+                        cadena = ""
+                        for i in range(4):
+                                cadena = cadena + chr(datos[i + (4*j)])        
+                        self.__arreglo_datos.append(cadena)
                 self.__flag_datos = 1                
 
         def print_datos(self):
-                print self.__x_acel
-                print self.__y_acel
-                print self.__z_acel
-                print self.__x_gyro
-                print self.__y_gyro
-                print self.__z_gyro
-
+                print self.__arreglo_datos
+                
         def obtener_datos(self):
                 self.send(chr(103))
                 while not self.__flag_datos:
                         pass
                 self.__flag_datos = 0
                 self.print_datos()
-                self.arreglo_datos.append(self.__x_acel)
-                self.arreglo_datos.append(self.__y_acel)
-                self.arreglo_datos.append(self.__z_acel)
-                self.arreglo_datos.append(self.__x_gyro)
-                self.arreglo_datos.append(self.__y_gyro)
-                self.arreglo_datos.append(self.__z_gyro)                
+                self.__x_acel = self.__arreglo_datos[0]
+                self.__y_acel = self.__arreglo_datos[1]
+                self.__z_acel = self.__arreglo_datos[2]
+                self.__x_gyro = self.__arreglo_datos[3]
+                self.__y_gyro = self.__arreglo_datos[4]
+                self.__z_gyro = self.__arreglo_datos[5]               
                 return self.__arreglo_datos
